dfe54d4fb5cfe25cb2005709d45d1d939f0ee1ca
leepuppychow <leepuppychow@gmail.com>
2019-01-29 00:02:32 -0700

    Figures out a greedy algorithm for Egyptian fractions

--- greedy/egyption_fraction.go
+++ greedy/egyption_fraction.go
@@ -6,3 +6,15 @@
 // Ex: 12/13 is 1/2 + 1/3 + 1/12 + 1/156
 
 package greedy
+
+type Fraction struct {
+	Numerator   int
+	Denominator int
+}
+
+// Algorithm (greedy)
+// 1. Start with current fraction and do D % N
+// 		A. if D % N == 0 then add unit fraction (1 / (D/N))
+// 		B. else if D % N != 0 then add unit fraction (1 / (1 + (D/N)))
+// 2. Get current fraction - last unit fraction
+// 3. Repeat step 1 for that remaining Fraction (break once D % N == 0)
