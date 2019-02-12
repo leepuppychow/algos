58680441eb6615155b7436c68d04474f1a9a16d7
Lee Chow <leechow@Lees-MacBook-Pro.local>
2019-02-12 10:00:20 -0700

    Adds naive search for string pattern

---
+++ strings/naive_test.go
@@ -0,0 +1,31 @@
+package strings
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestIncludesNaive(t *testing.T) {
+	text := "Hello jello"
+	pattern := "ello"
+	expected := []int{1, 7}
+	actual := IncludesNaive(text, pattern)
+
+	for i, _ := range expected {
+		if expected[i] != actual[i] {
+			t.Errorf("IncludesNaive failed")
+		}
+	}
+
+	pattern = "l"
+	expected = []int{2, 3, 8, 9}
+	actual = IncludesNaive(text, pattern)
+
+	for i, _ := range expected {
+		if expected[i] != actual[i] {
+			t.Errorf("IncludesNaive failed")
+		}
+	}
+	fmt.Println("Expected", expected)
+	fmt.Println("Actual", actual)
+}
