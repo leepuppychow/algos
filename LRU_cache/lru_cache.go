b2adadd634146bc6113a6085b70532a430e09b87
leepuppychow <leepuppychow@gmail.com>
2019-02-18 23:30:44 -0700

    Working on LRU cache

---
+++ LRU_cache/lru_cache.go
@@ -0,0 +1,18 @@
+package lru_cache
+
+type LRUCache struct {
+	Cache map[int]int
+	Queue []int
+}
+
+func Constructor(capacity int) LRUCache {
+
+}
+
+func (this *LRUCache) Get(key int) int {
+
+}
+
+func (this *LRUCache) Set(key, value int) int {
+
+}
