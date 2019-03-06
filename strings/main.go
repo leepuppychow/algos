7d6cd7884dc90274916b31a9c03389beda68016d
leepuppychow <leepuppychow@gmail.com>
2019-03-05 21:37:41 -0700

    Adds in longest palindrome problem... need to refactor

---
+++ strings/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/leepuppychow/algos/strings/palindrome"
+)
+
+func main() {
+	// fmt.Println(palindrome.IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(palindrome.LongestPalindrome("xxxaaxxx level"))
+}
