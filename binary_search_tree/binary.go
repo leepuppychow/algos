75536de98b52e04513c460e3022753a9031752d4
leepuppychow <leepuppychow@gmail.com>
2019-05-04 09:59:36 -0600

    Adds delete from BST in golang

--- binary_search_tree/binary.go
+++ binary_search_tree/binary.go
@@ -1,5 +1,9 @@
 package binary_search_tree
 
+import (
+	"strconv"
+)
+
 type Node struct {
 	Data  int
 	Left  *Node
@@ -7,7 +11,8 @@ type Node struct {
 }
 
 type BST struct {
-	Root *Node
+	Root   *Node
+	Output string
 }
 
 func Insert(data int, node *Node) {
@@ -36,20 +41,60 @@ func Insert(data int, node *Node) {
 	}
 }
 
-func Search(data int, current *Node) bool {
+func Search(data int, current, parent *Node) (bool, *Node, *Node) {
 	if current.Data == data {
-		return true
+		return true, current, parent
 	} else if current.Left == nil && current.Right == nil {
-		return false
+		return false, current, parent
 	}
 
 	if data < current.Data {
-		return Search(data, current.Left)
+		return Search(data, current.Left, current)
 	} else {
-		return Search(data, current.Right)
+		return Search(data, current.Right, current)
 	}
 }
 
-func (tree *BST) Delete() {
-	
+func Delete(data int, current *Node) bool {
+	found, node, parent := Search(data, current, nil)
+	if !found {
+		return false
+	}
+	if node.Right != nil {
+		temp := node.Right
+		temp.Left = node.Left
+		ShiftSubTree(temp, parent, false)
+	} else if node.Left != nil {
+		temp := node.Left
+		ShiftSubTree(temp, parent, false)
+	} else {
+		ShiftSubTree(node, parent, true)
+	}
+	return true
+}
+
+func ShiftSubTree(current, parent *Node, isLeaf bool) {
+	if current.Data > parent.Data {
+		if isLeaf {
+			parent.Right = nil
+		} else {
+			parent.Right = current
+		}
+	} else {
+		if isLeaf {
+			parent.Left = nil
+		} else {
+			parent.Left = current
+		}
+	}
+}
+
+func (tree *BST) SortedOutputAsc(current *Node) {
+	if current.Left != nil {
+		tree.SortedOutputAsc(current.Left)
+	}
+	tree.Output += strconv.Itoa(current.Data) + " "
+	if current.Right != nil {
+		tree.SortedOutputAsc(current.Right)
+	}
 }
